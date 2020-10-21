f15b0e316c90757b9df493121b1e484d95ae86e9
Deepanshu Panday <53929669+dipanshu765@users.noreply.github.com>
2020-10-21 19:04:25 +0530

    Update settings.py

--- shayari/settings.py
+++ shayari/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#6m*htn!1!-3b2xw9a^df!(^u#dr8_rw#nhsrd&doh2e9_a&f5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dpworldz.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
